5a1065edafed20fede87fa4be4f345874e8e493b
kazuya-nakayama <zkazun0824@yahoo.co.jp>
2017-08-15 14:32:15 +0900

    Crypto
    I mounted RSA-certification

--- PBFT/ModulePack.py
+++ PBFT/ModulePack.py
@@ -33,10 +33,16 @@ def msg_to_dict(message):
 
 def input_transmission_data():
     data = {}
-    data["title"] = "request"
     print ("If you want to stop, type \"end\"")
     print ("contents:", end="")
     data["contents"] = input()
+    if data["contents"] == "end":
+        return data
+    print ("type random 2 numbers")
+    data["num1"] = int(input())
+    data["num2"] = int(input())
+    print ("type low_limit of pub_key")
+    data["pub_low_lim"] = int(input())
     return data
 
 def get_address(address_file):
@@ -52,3 +58,70 @@ def write_ledger(contents):
     openledger = open("ledger.txt", "a")
     openledger.write(contents + "\n")
     openledger.close()
+
+def cer_add(text, key, mod_num):
+    decrypted_text = decrypt(text, key, mod_num)
+    if decrypted_text.startswith("From" + str(key) + ":"):
+        return True
+    else:
+        return False
+
+def encrypt(text, key, mod_num):
+    text_int = []
+    for char in text:
+        text_int.append(ord(char))
+    enc_text_int = []
+    for num in text_int:
+        enc_text_int.append(pow(num, key, mod_num))
+    enc_text_int_str = []
+    for enc_num in enc_text_int:
+        enc_text_int_str.append(str(enc_num))
+    enc_text = " ".join(enc_text_int_str)
+    return enc_text
+
+def decrypt(text, key, mod_num):
+    text_int_str = text.split(" ")
+    text_int = []
+    for num_str in text_int_str:
+        text_int.append(int(num_str))
+    dec_text_int = []
+    for num in text_int:
+        dec_text_int.append(pow(num, key, mod_num))
+    dec_text_list = []
+    for dec_num in dec_text_int:
+        dec_text_list.append(chr(dec_num))
+    dec_text = "".join(dec_text_list)
+    return dec_text
+
+def make_keys(p, q, pub_low_lim):
+    from math import gcd
+    mod_num = p * q
+    L = ((p - 1) * (q - 1)) // gcd(p - 1, q - 1)
+    for i in range(2, L):
+        if gcd(i, L) == 1 and i >= pub_low_lim:
+            pub_key = i
+            break
+    for i in range(2, L):
+        if (pub_key * i) % L == 1:
+            pri_key = i
+            break
+    return {"pri_key":pri_key, "pub_key":pub_key, "mod_num":mod_num}
+
+def make_prime_num(low_lim):
+    if low_lim == 2:
+        return 2
+    import math
+    p = low_lim
+    if (p % 2) == 0:
+        p += 1
+    while True:
+        s = 1
+        q = int(math.sqrt(p))
+        for r in range(3, q + 1):
+            if (p % r) == 0:
+                s = 0
+                break
+        if s == 1:
+            break
+        p += 2
+    return p
--- PBFT/PBFT.py
+++ PBFT/PBFT.py
@@ -21,18 +21,32 @@ def msg_reaction(client, server, rec_msg):
 
 def react_func(json_file):
     requestID = json_file["requestID"]
-    if json_file["title"] == "request":
-        request_data[requestID] = {"num_per":2, "contents":json_file["contents"]}
-        time.sleep(0.1)
-        ws_broadcast(ws_list, {"title":"react", "requestID":requestID})
-    if json_file["title"] == "react" and requestID in request_data:
-        request_data[requestID]["num_per"] += 1
-        if request_data[requestID]["num_per"] >= int(len(ws_list) * per_rate):
-            MP.write_ledger(request_data[requestID]["contents"])
-            ws_broadcast(ws_list, {"title":"wrote","requestID":requestID})
-            del(request_data[requestID])
+    title = json_file["title"]
     if json_file["title"] == "wrote" and requestID in wait_tra:
         wait_tra[requestID] += 1
+    if title == "request":
+        contents = json_file["contents"]
+        key = int(json_file["key"])
+        mod_num = int(json_file["mod_num"])
+        dec_con = MP.decrypt(contents, key, mod_num)
+        request_data[requestID] = {"num_per":1, "num_den":0, "contents":dec_con}
+        time.sleep(0.1)
+        if MP.cer_add(contents, key, mod_num):
+            ws_broadcast(ws_list, {"title":"react", "requestID":requestID, "permission":"permited"})
+        else:
+            ws_broadcast(ws_list, {"title":"react", "requestID":requestID, "permission":"denied"})
+    if json_file["title"] == "react" and requestID in request_data:
+        if json_file["permission"] == "permited":
+            request_data[requestID]["num_per"] += 1
+            if request_data[requestID]["num_per"] >= int(len(ws_list) * per_rate):
+                MP.write_ledger(request_data[requestID]["contents"])
+                ws_broadcast(ws_list, {"title":"wrote","requestID":requestID})
+                del(request_data[requestID])
+        if json_file["permission"] == "denied":
+            request_data[requestID]["num_den"] += 1
+            if request_data[requestID]["num_den"] >= int(len(ws_list) * (1 - per_rate)):
+                del(request_data[requestID])
+
 
 def ws_connect(address):
     ws_connect_list = []
@@ -76,11 +90,26 @@ if __name__ == "__main__":
     ws_list = ws_connect(address)
     request_num = 0
     while True:
-        tra_data = MP.input_transmission_data()
-        if tra_data["contents"] == "end":
+        input_data = MP.input_transmission_data()
+        if input_data["contents"] == "end":
             break
         else:
+            N1 = MP.make_prime_num(input_data["num1"])
+            N2 = MP.make_prime_num(input_data["num2"])
+            keys = MP.make_keys(N1, N2, input_data["pub_low_lim"])
+            print (keys)
+            pri_key = keys["pri_key"]
+            pub_key = keys["pub_key"]
+            mod_num = keys["mod_num"]
+            con = "From" + str(pub_key) + ":" + input_data["contents"]
+            print (con)
+            enc_con = MP.encrypt(con, pri_key, mod_num)
+            tra_data = {}
+            tra_data["title"] = "request"
             tra_data["requestID"] = address[0] + ":" + str(request_num)
+            tra_data["key"] = str(pub_key)
+            tra_data["mod_num"] = str(mod_num)
+            tra_data["contents"] = enc_con
             wait_tra[tra_data["requestID"]] = 0
             ws_broadcast(ws_list, tra_data)
             request_num += 1
